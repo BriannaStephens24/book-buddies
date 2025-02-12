5069fbde2964e08ff1afd195e0485c8d3da696eb
Brianna Stephens <briannaastephens24@gmail.com>
2025-02-12 10:11:55 -0700

    sign up page form created

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -1,9 +1,35 @@
 const SignUp = () => {
 
 return (
-
+<>
 
   <h1>Sign Up Here</h1>
+
+  <form>
+    <input 
+    placeholder="First Name"
+    />
+
+    <input
+    placeholder="Last Name"
+    />
+
+    <input 
+    placeholder="Email Address"
+    />
+
+    <input
+    placeholder="Phone Number"
+    />
+
+<button>Sign Up Now!</button>
+
+
+
+  </form>
+
+</>
+
 )
 
 
